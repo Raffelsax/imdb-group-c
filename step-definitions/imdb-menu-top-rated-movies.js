53ceeac52a67d03f92c5484960190060bbda3fe9
Mattias Garthe <mattiasgarthe@gmail.com>
2020-01-21 14:34:26 +0100

    I'm done writing the step-definitions for both Top Rated Movies and the dropdown box that sorts the list based on what you've chosen.

--- step-definitions/imdb-menu-top-rated-movies.js
+++ step-definitions/imdb-menu-top-rated-movies.js
@@ -2,9 +2,11 @@ let { $, sleep } = require('./funcs');
 
 module.exports = function () {
 
+    let sleepTime = 2000;
+
     this.Given(/^that I am at the IMDB homepage$/, async function () {
         await helpers.loadPage('https://imdb.com');
-        await sleep(2000);
+        await sleep(sleepTime);
         let logo = await $('#home_img_holder');
         assert(logo, 'Expected the IMDB logo to prove that I am on the IMDB website');
         
@@ -13,7 +15,7 @@ module.exports = function () {
     this.Given(/^I have clicked on the menu button$/, async function () {
         let button = await $('#imdbHeader-navDrawerOpen--desktop > div')
         button.click()
-        await sleep(2000)
+        await sleep(sleepTime)
         assert(button, '');
         
     });
@@ -21,7 +23,7 @@ module.exports = function () {
     this.Given(/^I have clicked on top rated movies$/, async function () {
         let linkElement = await driver.findElement(by.linkText("Top Rated Movies"))
         await linkElement.click();
-        await sleep(2000)
+        await sleep(sleepTime)
         
     });
 
@@ -33,16 +35,46 @@ module.exports = function () {
 
     
     this.When(/^I select sort by IMDB ratings in the sort\-by box$/, async function () {
-        let sortByDropDown = await $('#lister-sort-by-options > option:nth-child(2)'); 
-        sortByDropDown.click();
-        await sleep(2000);
-        assert(sortByDropDown, '')
+        let dropDownBox = await $('#lister-sort-by-options > option:nth-child(2)'); 
+        dropDownBox.click();
+        await sleep(sleepTime);
+        assert(dropDownBox, 'Expected the list to change to IMDB ratings');
 
     });
 
-   
+    
+    this.Then(/^the list should re\-sort by IMDB ratings$/, async function () {
+        driver.navigate().refresh();
+        let dropDownOption = await driver.findElement(by.css('#lister-sort-by-options option[selected]')).getText();
+        assert('IMDB Rating', dropDownOption);
+        
+    }); 
 
+    this.When(/^I select sort by release date in the sort\-by box$/, async function () {
+        let dropDownBox = await $('#lister-sort-by-options > option:nth-child(3)');
+        dropDownBox.click();
+        await sleep(sleepTime);
+        assert(dropDownBox, 'Expected the list to change to release dates');
+    });
 
+    this.Then(/^the list should re\-sort by release date$/, async function () {
+        driver.navigate().refresh();
+        let dropDownOption = await driver.findElement(by.css('#lister-sort-by-options option[selected]')).getText();
+        assert('Release Date', dropDownOption);
+    });
+
+    this.When(/^I select sort by number of ratings in the sort\-by box$/, async function () {
+        let dropDownBox = await $('#lister-sort-by-options > option:nth-child(4)');
+        dropDownBox.click();
+        await sleep(sleepTime);
+        assert(dropDownBox, 'Expected the list to change to number of ratings');
+    });
+
+    this.Then(/^the list should re\-sort by number of ratings$/, async function () {
+        driver.navigate().refresh();
+        let dropDownOption = await driver.findElement(by.css('#lister-sort-by-options option[selected]')).getText();
+        assert('Number of Ratings', dropDownOption);
+    });
 
 
 }
