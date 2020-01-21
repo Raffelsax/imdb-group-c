30f6c87d196a843ec26d0c9a755665542c6faba5
Anton Fransén <ante.fransen@gmail.com>
2020-01-21 15:21:27 +0100

    i but a bunch off asserts in my code

--- step-definitions/imbd-signin.js
+++ step-definitions/imbd-signin.js
@@ -14,27 +14,31 @@ module.exports = function () {
 
         let signInButtons = await driver.findElements(by.css('.auth-provider-text'));
         let signInWithIMDbButton = signInButtons[0];
+        assert(signInWithIMDbButton, 'expect the button to exist');
         await signInWithIMDbButton.click();
         await driver.findElement(by.css('#ap_email'));
+        assert(signInWithIMDbButton, 'expect the input box for the email');
     });
 
     this.When(/^type in the email : "([^"]*)"$/, async function (email) {
 
         let emailFeild = await driver.findElement(by.css('#ap_email'));
         await emailFeild.sendKeys(email);
+        assert(emailFeild, 'expect the input box for the email');
     });
 
     this.When(/^type in the password : "([^"]*)"$/, async function (password) {
 
         let passwordFeild = await driver.findElement(by.css('#ap_password'));
         await passwordFeild.sendKeys(password);
-        await sleep(2000);
+        assert(passwordFeild, 'expect the input box for the password');
     });
 
     this.When(/^type in the password : "([^"]*)" \+ hit ENTER$/, async function (password) {
 
         let passwordFeild = await driver.findElement(by.css('#ap_password'));
         await passwordFeild.sendKeys(password);
+        assert(passwordFeild, 'expect the input box for the email, to be '+ password);
         await passwordFeild.sendKeys(selenium.Key.ENTER);
     });
 
@@ -42,6 +46,7 @@ module.exports = function () {
 
         let signInButton = await driver.findElement(by.css('#signInSubmit'));
         await signInButton.click();
+        assert(signInButton, 'expact to get signd in after clicking the sing in button');
     });
 
     this.Then(/^I should get singnd in$/, async function () {
